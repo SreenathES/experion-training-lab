e83d448e06800aabaf13028b99a9ecafb413fe97
Sreenath E S <sreenath.es@experionglobal.local>
2023-01-16 10:52:07 +0530

    JS lab files: 03-01-2023

---
+++ JS 03-01-2023/json-serialization.js
@@ -0,0 +1,23 @@
+const student = [
+    {
+        name: 'Tony',
+        age: 18,
+        mark1: 10,
+        mark2: 7
+    },
+    {
+        name: 'Clint',
+        age: 18,
+        mark1: 10,
+        mark2: 7
+    },
+    {
+        name: 'Steve',
+        age: 18,
+        mark1: 10,
+        mark2: 7
+    },
+];
+
+const data = JSON.stringify(student);
+console.log(data);
